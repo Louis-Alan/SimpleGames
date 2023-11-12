8c3431981db62ad994def8d24cb5adb26ab0549b
Alan Louis <alouis811@gmail.com>
2023-11-12 23:38:30 +0530

    Kill enemy on click, delete enemies on out of bound

--- FirstGame/src/Game.cpp
+++ FirstGame/src/Game.cpp
@@ -3,14 +3,13 @@
 #include <ctime>
 #include <vector>
 
-#include "SFML/Window/Mouse.hpp"
 #include "celeste_lib.h"
 
 // Private Functions
 void Game::initVariables() {
   this->window = nullptr;
   this->points = 0;
-  this->enemySpawnTimerMax = 1000.f;
+  this->enemySpawnTimerMax = 10.f;
   this->enemySpawnTimer = this->enemySpawnTimerMax;
   this->maxEnemies = 5;
 }
@@ -39,10 +38,14 @@ Game::Game() {
   this->initEnemies();
 }
 
-Game::~Game() { delete this->window; }
+Game::~Game() {
+  delete this->window;
+}
 
-// Acessors
-const bool Game::isWindowOpen() const { return this->window->isOpen(); }
+// Accessors
+const bool Game::isWindowOpen() const {
+  return this->window->isOpen();
+}
 
 // Public Functions
 
@@ -50,7 +53,7 @@ void Game::spawnEnemy() {
   /*
    *  @return void
    *
-   *  Spawns enemies and sets thier colors and positions
+   *  Spawns enemies and sets their colors and positions
    *  -Sets a random position.
    *  -Sets a random color.
    *  -Adds enemy to the vector
@@ -70,20 +73,21 @@ void Game::spawnEnemy() {
 void Game::pollEvents() {
   while (this->window->pollEvent(this->ev)) {
     switch (this->ev.type) {
-    case sf::Event::Closed:
-      this->window->close();
-      break;
-    case sf::Event::KeyPressed:
-      if (this->ev.key.code == sf::Keyboard::Escape) {
+      case sf::Event::Closed:
         this->window->close();
-      }
-      break;
+        break;
+      case sf::Event::KeyPressed:
+        if (this->ev.key.code == sf::Keyboard::Escape) {
+          this->window->close();
+        }
+        break;
     }
   }
 }
 
 void Game::updateMousePositions() {
   this->mousePosWindow = sf::Mouse::getPosition(*this->window);
+  this->mousePosView = this->window->mapPixelToCoords(this->mousePosWindow);
 }
 
 void Game::updateEnemies() {
@@ -95,7 +99,7 @@ void Game::updateEnemies() {
    * Removes the enemies at edge of screen
    */
 
-  // updating timer for enemmy spawn
+  // updating timer for enemy spawn
   if (this->enemies.size() < this->maxEnemies) {
     if (this->enemySpawnTimer >= this->enemySpawnTimerMax) {
       // spawn enemy reset timer
@@ -106,16 +110,33 @@ void Game::updateEnemies() {
     }
   }
 
-  // Move the enemies
-  for (auto &e : this->enemies) {
-    e.move(0.f, 5.f);
+  // Move and updating enemies
+  for (int i = 0; i < this->enemies.size(); i++) {
+    bool deleted = false;
+    this->enemies[i].move(0.f, 1.f);
+
+    // Check if clicked upon
+    if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+      if (this->enemies[i].getGlobalBounds().contains(this->mousePosView)) {
+        deleted = true;
+      }
+    }
+
+    // If enemy is past bottom of the screen
+    if (this->enemies[i].getPosition().y > this->window->getSize().y) {
+      deleted = true;
+    }
+
+    if (deleted) {
+      this->enemies.erase(this->enemies.begin() + i);
+    }
   }
 }
 
 void Game::update() {
   this->pollEvents();
 
-  // Update Mouse Positiion
+  // Update Mouse Position
   this->updateMousePositions();
 
   this->updateEnemies();
@@ -123,7 +144,7 @@ void Game::update() {
 
 void Game::renderEnemies() {
   // Rendering all enemies
-  for (auto &e : this->enemies) {
+  for (auto& e : this->enemies) {
     this->window->draw(e);
   }
 }
